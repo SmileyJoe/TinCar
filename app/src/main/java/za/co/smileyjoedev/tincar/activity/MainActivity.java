081601c328125aa95d4885bfd90d91d6630d8221
SmileyJoe <cody.strugnell@gmail.com>
2016-08-11 22:08:39 +0200

    Implementing base swipe cards

--- app/src/main/java/za/co/smileyjoedev/tincar/activity/MainActivity.java
+++ app/src/main/java/za/co/smileyjoedev/tincar/activity/MainActivity.java
@@ -6,21 +6,65 @@ import android.util.Log;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import java.util.ArrayList;
 
 import za.co.smileyjoedev.tincar.R;
+import za.co.smileyjoedev.tincar.adapter.SwipeCardAdapter;
 import za.co.smileyjoedev.tincar.backend.Api;
 import za.co.smileyjoedev.tincar.backend.ApiCallback;
 import za.co.smileyjoedev.tincar.object.Car;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SwipeCardAdapter mSwipeCardAdapterCar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupViews();
         Api.getCars(this, new GetCarCallback(), true);
+    }
+
+    private void setupViews(){
+        mSwipeCardAdapterCar = new SwipeCardAdapter(getBaseContext(), new ArrayList<Car>());
+        SwipeFlingAdapterView swipeView = (SwipeFlingAdapterView) findViewById(R.id.swipe_cars);
+        swipeView.setAdapter(mSwipeCardAdapterCar);
+        swipeView.setFlingListener(new CarFlingListener());
+    }
+
+    private class CarFlingListener implements SwipeFlingAdapterView.onFlingListener{
+
+        private Car mCarRemoved;
+
+        @Override
+        public void removeFirstObjectInAdapter() {
+            Log.d("TinCar", "remove");
+            mCarRemoved = mSwipeCardAdapterCar.getItem(0);
+            mSwipeCardAdapterCar.remove(0);
+        }
+
+        @Override
+        public void onLeftCardExit(Object o) {
+            Log.d("TinCar", "left: " + mCarRemoved.getTitle());
+        }
+
+        @Override
+        public void onRightCardExit(Object o) {
+            Log.d("TinCar", "right: " + mCarRemoved.getTitle());
+        }
+
+        @Override
+        public void onAdapterAboutToEmpty(int i) {
+            Log.d("TinCar", "empty");
+        }
+
+        @Override
+        public void onScroll(float v) {
+            Log.d("TinCar", "scroll");
+        }
     }
 
     private class GetCarCallback extends ApiCallback{
@@ -33,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
                 if(element.isJsonArray()){
                     ArrayList<Car> cars = Car.fromApiResponse(element.getAsJsonArray());
-
+                    mSwipeCardAdapterCar.update(cars);
                     Log.d("TinCar", cars.get(0).toString());
                 }
             }
---
+++ app/src/main/java/za/co/smileyjoedev/tincar/adapter/SwipeCardAdapter.java
@@ -0,0 +1,93 @@
+package za.co.smileyjoedev.tincar.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.koushikdutta.ion.Ion;
+
+import java.util.ArrayList;
+
+import za.co.smileyjoedev.tincar.R;
+import za.co.smileyjoedev.tincar.object.Car;
+
+/**
+ * Created by cody on 2016/08/11.
+ */
+public class SwipeCardAdapter extends BaseAdapter {
+
+    private ArrayList<Car> mItems = new ArrayList<>();
+    private Context mContext;
+
+    public SwipeCardAdapter(Context context, ArrayList<Car> items) {
+        mContext = context;
+        mItems = items;
+    }
+
+    public void update(ArrayList<Car> cars){
+        mItems = cars;
+        notifyDataSetChanged();
+    }
+
+    public void remove(int position){
+        mItems.remove(position);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return mItems.size();
+    }
+
+    @Override
+    public Car getItem(int position) {
+        return mItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return getItem(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.swipe_card_item_car, parent, false);
+
+            holder = new ViewHolder();
+            holder.process(convertView);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.render(getItem(position));
+
+        return convertView;
+    }
+
+    private static class ViewHolder{
+        private TextView mTextTitle;
+        private ImageView mImageCar;
+
+        public void process(View view){
+            mTextTitle = (TextView) view.findViewById(R.id.text_title);
+            mImageCar = (ImageView) view.findViewById(R.id.image_car);
+        }
+
+        public void render(Car car){
+            mTextTitle.setText(car.getTitle());
+            Ion.with(mImageCar)
+                    .placeholder(android.R.drawable.btn_star_big_off)
+                    .error(android.R.drawable.btn_star_big_on)
+//                    .animateLoad(spinAnimation)
+//                    .animateIn(fadeInAnimation)
+                    .load(car.getDefaultImageUrl());
+        }
+    }
+}
